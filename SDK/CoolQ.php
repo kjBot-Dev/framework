614fda98f5f72cfdcd948ff641cb3b7a36741427
kj415j45 <kj415j45@gmail.com>
2021-04-10 08:04:32 +0000

    Update to OneBot v11

--- SDK/CoolQ.php
+++ SDK/CoolQ.php
@@ -407,14 +407,33 @@ class CoolQ{
         }
         $result = json_decode(file_get_contents('http://'.$this->host.$api.$queryStr));
 
-        switch($result->retcode){
-            case 0:
-                return $result->data;
-            case 1:
-                return NULL;
-            default:
-                throw new \Exception("Query Failed", $result->retcode);
+        $responseCode = $this->parseHeaders($http_response_header)['reponse_code'];
+        if($responseCode === 200){
+            switch($result->retcode){
+                case 0:
+                    return $result->data;
+                case 1:
+                    return NULL;
+                default:
+                    throw new \Exception("Query Failed", $result->retcode);
+            }
         }
+        throw new \Exception("Query Failed", $responseCode);
+    }
+
+    private function parseHeaders($headers){
+        $head = [];
+        foreach($headers as $k => $v){
+            $t = explode(':', $v, 2);
+            if(isset($t[1])){
+                $head[trim($t[0])] = trim($t[1]);
+            }else{
+                $head[]= $v;
+                if(preg_match("#HTTP/[0-9\.]+\s+([0-9]+)#",$v, $out))
+                    $head['reponse_code'] = intval($out[1]);
+            }
+        }
+        return $head;
     }
 
 }
