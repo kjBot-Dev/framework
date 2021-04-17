aa3fa468b605cf77d40dcf34d17c1df2be0e6a40
ZiBuYuCHN <1063614727@qq.com>
2021-04-18 04:30:22 +0800

    去除composer

--- public/init.php
+++ public/init.php
@@ -3,6 +3,8 @@
 include_once('../plugins.php');
 include_once('../modules.php');
 include_once('../framework/Loader.php');
+include_once('../framework/helpers.php');
+include_once('../framework/functions.php');
 spl_autoload_register('\kjBot\Framework\Loader::autoload');
 
 
