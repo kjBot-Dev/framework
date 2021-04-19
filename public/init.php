e2d330850681e4f5e12423466c0d0eab58365bca
ZiBuYuCHN <1063614727@qq.com>
2021-04-19 11:14:20 +0800

    fix

--- public/init.php
+++ public/init.php
@@ -4,7 +4,6 @@ include_once('../plugins.php');
 include_once('../modules.php');
 include_once('../framework/Loader.php');
 include_once('../framework/helpers.php');
-include_once('../framework/functions.php');
 spl_autoload_register('\kjBot\Framework\Loader::autoload');
 
 
